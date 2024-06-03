0a07cd983bcce05c70bd441a8786b8a2deac9ee0
lucpv <lucpv@vng.com.vn>
2024-06-03 16:16:03 +0700

    update flyway

--- src/main/resources/migration/V3__Insert_table.sql
+++ src/main/resources/migration/V3__Insert_table.sql
@@ -1,5 +1,13 @@
 -- Change log: V3
--- Description: Insert table
+-- Description: Create and insert table surveys
+
+CREATE TABLE surveys (
+    survey_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
 
 INSERT INTO surveys (title, description, created_at, updated_at) VALUES
 ('Sàng lọc chung', 'Qui trình tham vấn', NOW(), NOW());
