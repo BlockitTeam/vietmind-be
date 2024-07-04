3fac9bf502b3b23eae68526b7a8866d4e2598d09
hunterpvl <hunterpvl@gmail.com>
2024-07-05 01:11:17 +0700

    remove page login default

--- src/main/java/com/vm/config/WebSecurityConfig.java
+++ src/main/java/com/vm/config/WebSecurityConfig.java
@@ -1,10 +1,8 @@
 package com.vm.config;
 
-import com.vm.constant.Provider;
 import com.vm.handler.CustomAuthenticationSuccessHandler;
 import com.vm.service.CustomOAuth2UserService;
 import com.vm.service.UserService;
-import com.vm.service.impl.CustomOAuth2User;
 import com.vm.service.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -15,7 +13,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -26,10 +23,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Arrays;
 
 @Configuration
@@ -86,34 +80,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/v1/auth", "/api/v1/auth/login", "/api/v1/auth/test").permitAll() // Allow access to /api/auth/token without authentication
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().permitAll()
-                .loginPage("/login")
-                .usernameParameter("email")
-                .passwordParameter("pass")
-                .successHandler(customAuthenticationSuccessHandler())
-                .and()
-                .oauth2Login()
-                .loginPage("/login")
-                .userInfoEndpoint()
-                .userService(oauthUserService)
-                .and()
-                .successHandler(new AuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                                        Authentication authentication) throws IOException, ServletException {
-                        System.out.println("AuthenticationSuccessHandler invoked");
-                        System.out.println("Authentication name: " + authentication.getName());
-                        CustomOAuth2User oauthUser = (CustomOAuth2User) authentication.getPrincipal();
-                        try {
-                            userService.processOAuthPostLogin(oauthUser.getEmail(), Provider.LOCAL);
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
-                        response.sendRedirect("/api/v1/user/current-user");
-                    }
-                })
-                //.defaultSuccessUrl("/list")
-                .and()
                 .logout()
                 .logoutUrl("/api/v1/logout")
                 .addLogoutHandler(logoutHandler())
@@ -153,7 +119,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Adjust this as per your frontend origin
+        configuration.setAllowedOrigins(Arrays.asList("http://91.108.104.57")); // Adjust this as per your frontend origin
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
         configuration.setAllowCredentials(true); // This is important to allow credentials (cookies) to be sent
