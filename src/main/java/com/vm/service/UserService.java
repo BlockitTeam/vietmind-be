9de869ae7336ded9681788706c4db625bf6c92d9
hunterpvl <hunterpvl@gmail.com>
2024-07-07 01:02:56 +0700

    Update UserService.java

--- src/main/java/com/vm/service/UserService.java
+++ src/main/java/com/vm/service/UserService.java
@@ -52,7 +52,8 @@ public class UserService {
 
 	public User getCurrentUser(String username) {
 		User existUser = userRepo.getUserByUsername(username);
-		existUser.setPassword(null);
+		if (existUser != null)
+			existUser.setPassword(null);
 		return existUser;
 	}
 
