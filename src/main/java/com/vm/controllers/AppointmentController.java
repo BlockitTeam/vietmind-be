f67962e7b78682c5dbfd8549a776a92b3edfe58e
hunterpvl <hunterpvl@gmail.com>
2024-07-07 22:52:57 +0700

    Update AppointmentController.java

--- src/main/java/com/vm/controllers/AppointmentController.java
+++ src/main/java/com/vm/controllers/AppointmentController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/appointments")
+@RequestMapping("/api/v1/appointments")
 @RequiredArgsConstructor
 public class AppointmentController {
     private final Logger log = LoggerFactory.getLogger(AppointmentController.class);
