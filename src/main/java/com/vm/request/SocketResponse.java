e3e4935acacde38030e5aaf1b1ed01d61c706df5
lucpv <lucpv@vng.com.vn>
2024-07-13 14:13:32 +0700

    update due to error in createAt of SocketResponse, ObjectMapper not support LocalDatetime Java 8

--- src/main/java/com/vm/request/SocketResponse.java
+++ src/main/java/com/vm/request/SocketResponse.java
@@ -1,5 +1,6 @@
 package com.vm.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.*;
@@ -17,6 +18,8 @@ public class SocketResponse {
     private String message;
     private int messageId;
     private String type;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime createAt;
 
     @SneakyThrows
