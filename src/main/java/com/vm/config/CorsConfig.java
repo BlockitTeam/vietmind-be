5181c24fd42f3a0f64f194233275e5002eb6036b
lucpv <lucpv@vng.com.vn>
2024-12-29 17:21:26 +0700

    add more CORS https

--- src/main/java/com/vm/config/CorsConfig.java
+++ src/main/java/com/vm/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://91.108.104.57", "http://localhost:3000", "http://vietmind.chat")
+                .allowedOriginPatterns("http://91.108.104.57", "http://localhost:3000", "http://vietmind.chat", "https://vietmind.chat")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
